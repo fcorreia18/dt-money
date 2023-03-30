daed4776911abebd5ed437e4f7f1b1cc05f9a719
fcorreia18 <francisco.correia1804@gmail.com>
2023-03-30 08:48:08 +0100

    chore: react-modal type definitions add

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,23 @@
-import { Atom, CurrencyCircleDollar } from 'phosphor-react';
-import React from 'react';
+import { CurrencyCircleDollar } from 'phosphor-react';
+import Modal from 'react-modal'
+import React, { useState } from 'react';
 import { Container, Content } from './styles';
+import ReactModal from 'react-modal';
 
 
 export const Header: React.FC = () => {
+    const [isNewTransactionModal, setIsNewTransactionModal ] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModal(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModal(false);
+    }
     return (
         <Container>
             <Content>
-
                 <span>
                     <CurrencyCircleDollar 
                         width={30} 
@@ -17,8 +27,13 @@ export const Header: React.FC = () => {
                     dt money
                 </span>
                 <button type='button'>nova transação</button>
-            </Content>
+                <ReactModal 
+                isOpen={isNewTransactionModal}
+                onRequestClose={handleCloseNewTransactionModal}
+                >
 
+                </ReactModal>
+            </Content>
         </Container>
 
     );
